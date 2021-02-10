1cddb73a999e656e962a280a79fe25d6ba012365
Oleksandr <gostar1337@gmail.com>
2021-02-10 09:40:15 +0200

    task5

--- practice1_11.js
+++ practice1_11.js
@@ -1,11 +1,19 @@
 const seconds = (total) => total % 60;
 const perimetr = (side, count) => side * count;
 const FBF = (n) => {
-    for (let i = 1; i < n; i++) {
-        let log = '';
-        if (n%3===0) log+="fizz";
-        if(n%5===0) log+="fizzbuzz";
-        console.log(log);
-    }
+  for (let i = 1; i < n; i++) {
+    let log = "";
+    if (n % 3 === 0) log += "fizz";
+    if (n % 5 === 0) log += "fizzbuzz";
+    console.log(log);
+  }
 };
-const calculate = (a,b,c) => (a+b+c)/3;
+const calculate = (a, b, c) => (a + b + c) / 3;
+const isDivisibleIF = (n, x, y) => {
+  if (n % x === 0 && n % y === 0) {
+    return true;
+  } else return false;
+};
+const isDivisibleTERN = (n, x, y) =>
+  n % x === 0 ? (n % y === 0 ? true : false) : false;
+const isDivisibleWithout = (n, x, y) => n % x === 0 && n % y === 0;
