23a460564e23b91754d92fa5dfc178640e4eafc2
Oleksandr <gostar1337@gmail.com>
2021-02-10 09:32:03 +0200

    task3

--- practice1_11.js
+++ practice1_11.js
@@ -1,4 +1,11 @@
 const seconds = (total) => total % 60;
 const perimetr = (side, count) => side * count;
-
+const FBF = (n) => {
+    for (let i = 1; i < n; i++) {
+        let log = '';
+        if (n%3===0) log+="fizz";
+        if(n%5===0) log+="fizzbuzz";
+        console.log(log);
+    }
+};
 
